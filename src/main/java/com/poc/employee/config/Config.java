93d86c280a00bb9eaff85b621eed6bcaf9d98169
Ramitha <ramitha.su@gmail.com>
2018-06-15 23:21:09 -0400

    Added cloud dependencies

---
+++ src/main/java/com/poc/employee/config/Config.java
@@ -0,0 +1,21 @@
+package com.poc.employee.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "bean")
+public class Config {
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message = "a message that can be changed live";
+
+
+}
