6d4320f2270b5044a60d47c9372204d88311e2a3
Ramitha <ramitha.su@gmail.com>
2018-06-15 23:30:18 -0400

    Added cloud dependencies

--- src/main/java/com/poc/employee/config/Config.java
+++ src/main/java/com/poc/employee/config/Config.java
@@ -1,9 +1,11 @@
 package com.poc.employee.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RefreshScope
 @ConfigurationProperties(prefix = "bean")
 public class Config {
 
